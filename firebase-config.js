3be9e8633eee852e37b645d85116454a1a315114
Rocket-Glitch <jhordanjovy@gmail.com>
2026-07-01 23:01:51 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// REEMPLAZA con tu configuración de Firebase
+export const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  databaseURL: "https://TU_PROYECTO-default-rtdb.firebaseio.com",
+  projectId: "TU_PROYECTO",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "000000000",
+  appId: "TU_APP_ID"
+};
