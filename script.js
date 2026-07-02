b12266edf2f782e7623dca13e6ed7b48189e3d55
Rocket-Glitch <jhordanjovy@gmail.com>
2026-07-01 22:34:43 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// Variable
+let player = "Yor";
+
+// Mostrar una alerta al abrir la página
+alert("Welcome!");
+
+// Función
+function hello() {
+    alert("Hello " + player + "!");
+}
+
+// Ejecutar la función
+hello();
