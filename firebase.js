87ebf3911bb318487056144dd6a0e74b53b22cdd
Rocket-Glitch <jhordanjovy@gmail.com>
2026-07-01 23:31:17 -0500

    Delete firebase.js

--- firebase.js
+++
@@ -1,4 +0,0 @@
-import {firebaseConfig} from './firebase-config.js';
-
-// Aquí debes importar Firebase desde CDN e inicializar la app.
-// El proyecto está preparado para ello, solo falta pegar tu configuración.
