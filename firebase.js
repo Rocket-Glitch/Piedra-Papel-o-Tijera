ab0bf161e636d971c9461645280736466de4d784
Rocket-Glitch <jhordanjovy@gmail.com>
2026-07-01 23:00:29 -0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,4 @@
+import {firebaseConfig} from './firebase-config.js';
+
+// Aquí debes importar Firebase desde CDN e inicializar la app.
+// El proyecto está preparado para ello, solo falta pegar tu configuración.
