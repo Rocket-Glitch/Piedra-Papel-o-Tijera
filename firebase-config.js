8c58e8b2c40956c5867e17bc7e33c147eaf2d401
Rocket-Glitch <jhordanjovy@gmail.com>
2026-07-02 00:18:28 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCVCz_E8YfpRttyq3g6ZkeWRlS5y9AaXKE",
   authDomain: "piedra-papel-o-tijera-1.firebaseapp.com",
-  databaseURL: "https://piedra-papel-o-tijera-1-default-rtdb.firebaseio.com", // ← ¡AGREGADO!
+  databaseURL: "https://piedra-papel-o-tijera-1-default-rtdb.firebaseio.com",
   projectId: "piedra-papel-o-tijera-1",
   storageBucket: "piedra-papel-o-tijera-1.firebasestorage.app",
   messagingSenderId: "147138851043",
   appId: "1:147138851043:web:8d9d87d033b4bf15e615d6"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
