7c5538df22a5e630eb654548b0ef35ce9fa0a4c9
Rocket-Glitch <jhordanjovy@gmail.com>
2026-07-01 23:31:35 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,27 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
 
-import {
-    getDatabase
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
-
-const firebaseConfig = {
-
-    apiKey: "AIzaSyCVCz_E8YfpRttyq3g6ZkeWRLS5y9AaXKE",
-
-    authDomain: "piedra-papel-o-tijera-1.firebaseapp.com",
-
-    databaseURL: "https://piedra-papel-o-tijera-1-default-rtdb.firebaseio.com",
-
-    projectId: "piedra-papel-o-tijera-1",
-
-    storageBucket: "piedra-papel-o-tijera-1.firebasestorage.app",
-
-    messagingSenderId: "147138851043",
-
-    appId: "1:147138851043:web:8d9d87d033b4bf15e615d6"
-
+const firebaseConfig={
+ apiKey:"REEMPLAZAR",
+ authDomain:"REEMPLAZAR",
+ databaseURL:"REEMPLAZAR",
+ projectId:"REEMPLAZAR",
+ storageBucket:"REEMPLAZAR",
+ messagingSenderId:"REEMPLAZAR",
+ appId:"REEMPLAZAR"
 };
 
-const app = initializeApp(firebaseConfig);
-
-export const db = getDatabase(app);
+const app=initializeApp(firebaseConfig);
+export const db=getDatabase(app);
